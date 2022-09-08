e1b091d4f18a2476fbd86c0b51b958bbf2f4f9d7
Iury Vasconcelos <iury0393@gmail.com>
2022-09-08 13:02:29 -0300

    first commit

---
+++ InspoQuotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InspoQuotes
+//
+//  Created by Iury Vasconcelos on 08/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
